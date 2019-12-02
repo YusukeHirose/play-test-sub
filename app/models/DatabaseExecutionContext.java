402583719daa37c33e604a80453bca24509fa463
hiroseyusuke <hiroseyusuke@team-lab.com>
2019-12-02 22:27:23 +0900

    fix: name削除

--- app/models/DatabaseExecutionContext.java
+++ app/models/DatabaseExecutionContext.java
@@ -9,7 +9,7 @@ import play.libs.concurrent.CustomExecutionContext;
 public class DatabaseExecutionContext extends CustomExecutionContext {
 
   @Inject
-  public DatabaseExecutionContext(ActorSystem actorSystem, String name) {
+  public DatabaseExecutionContext(ActorSystem actorSystem) {
     super(actorSystem, "database.dispatcher");
   }
 }
